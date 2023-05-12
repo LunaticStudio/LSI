2d8c9259ff83a38b6fe0159bbcc4ec180a514b81
LunaticStudio <victormgomescg@gmail.com>
2023-05-12 09:38:46 -0300

    BCS

---
+++ BCS/packs/all/kitchen 2 2023 - loaf.lua
@@ -0,0 +1,465 @@
+    {
+        label = "Kitchen 2 2023",
+        furniture = {
+            {
+                label = "Cabinet 1 A",
+                object = "ls_kitcol2_cabint1_a",
+                price = 750,
+            },
+            {
+                label = "Cabinet 1 B",
+                object = "ls_kitcol2_cabint1_b",
+                price = 750,
+            },
+            {
+                label = "Cabinet 1 C",
+                object = "ls_kitcol2_cabint1_c",
+                price = 750,
+            },
+            {
+                label = "Cabinet 1 D",
+                object = "ls_kitcol2_cabint1_d",
+                price = 750,
+            },
+            {
+                label = "Cabinet 2 A",
+                object = "ls_kitcol2_cabint2_a",
+                price = 750,
+            },
+            {
+                label = "Cabinet 2 B",
+                object = "ls_kitcol2_cabint2_b",
+                price = 750,
+            },
+            {
+                label = "Cabinet 2 C",
+                object = "ls_kitcol2_cabint2_c",
+                price = 750,
+            },
+            {
+                label = "Cabinet 2 D",
+                object = "ls_kitcol2_cabint2_d",
+                price = 750,
+            },
+            {
+                label = "Refrigerator A",
+                object = "ls_kitcol2_refrig1_a",
+                price = 750,
+            },
+            {
+                label = "Refrigerator B",
+                object = "ls_kitcol2_refrig1_b",
+                price = 750,
+            },
+            {
+                label = "Refrigerator C",
+                object = "ls_kitcol2_refrig1_c",
+                price = 750,
+            },
+            {
+                label = "Refrigerator D",
+                object = "ls_kitcol2_refrig1_d",
+                price = 750,
+            },
+            {
+                label = "Stove Hood A",
+                object = "ls_kitcol2_shood1_a",
+                price = 750,
+            },
+            {
+                label = "Stove Hood B",
+                object = "ls_kitcol2_shood1_b",
+                price = 750,
+            },
+            {
+                label = "Stove Hood C",
+                object = "ls_kitcol2_shood1_c",
+                price = 750,
+            },
+            {
+                label = "Stove Hood D",
+                object = "ls_kitcol2_shood1_d",
+                price = 750,
+            },
+            {
+                label = "Stool 1 A",
+                object = "ls_kitcol2_stool1_a",
+                price = 750,
+            },
+            {
+                label = "Stool 1 B",
+                object = "ls_kitcol2_stool1_b",
+                price = 750,
+            },
+            {
+                label = "Stool 1 C",
+                object = "ls_kitcol2_stool1_c",
+                price = 750,
+            },
+            {
+                label = "Stool 1 D",
+                object = "ls_kitcol2_stool1_d",
+                price = 750,
+            },
+            {
+                label = "Stove A",
+                object = "ls_kitcol2_stove1_a",
+                price = 750,
+            },
+            {
+                label = "Stove B",
+                object = "ls_kitcol2_stove1_b",
+                price = 750,
+            },
+            {
+                label = "Stove C",
+                object = "ls_kitcol2_stove1_c",
+                price = 750,
+            },
+            {
+                label = "Stove D",
+                object = "ls_kitcol2_stove1_d",
+                price = 750,
+            },
+            {
+                label = "Counter Type 10 A",
+                object = "ls_kitcol2_counter0_a",
+                price = 750,
+            },
+            {
+                label = "Counter Type 10 B",
+                object = "ls_kitcol2_counter0_b",
+                price = 750,
+            },
+            {
+                label = "Counter Type 10 C",
+                object = "ls_kitcol2_counter0_c",
+                price = 750,
+            },
+            {
+                label = "Counter Type 10 D",
+                object = "ls_kitcol2_counter0_d",
+                price = 750,
+            },
+            {
+                label = "Counter Type 1 A",
+                object = "ls_kitcol2_counter1_a",
+                price = 750,
+            },
+            {
+                label = "Counter Type 1 B",
+                object = "ls_kitcol2_counter1_b",
+                price = 750,
+            },
+            {
+                label = "Counter Type 1 C",
+                object = "ls_kitcol2_counter1_c",
+                price = 750,
+            },
+            {
+                label = "Counter Type 1 D",
+                object = "ls_kitcol2_counter1_d",
+                price = 750,
+            },
+            {
+                label = "Counter Type 2 A",
+                object = "ls_kitcol2_counter2_a",
+                price = 750,
+            },
+            {
+                label = "Counter Type 2 B",
+                object = "ls_kitcol2_counter2_b",
+                price = 750,
+            },
+            {
+                label = "Counter Type 2 C",
+                object = "ls_kitcol2_counter2_c",
+                price = 750,
+            },
+            {
+                label = "Counter Type 2 D",
+                object = "ls_kitcol2_counter2_d",
+                price = 750,
+            },
+            {
+                label = "Counter Type 3 A",
+                object = "ls_kitcol2_counter3_a",
+                price = 750,
+            },
+            {
+                label = "Counter Type 3 B",
+                object = "ls_kitcol2_counter3_b",
+                price = 750,
+            },
+            {
+                label = "Counter Type 3 C",
+                object = "ls_kitcol2_counter3_c",
+                price = 750,
+            },
+            {
+                label = "Counter Type 3 D",
+                object = "ls_kitcol2_counter3_d",
+                price = 750,
+            },
+            {
+                label = "Counter Type 4 A",
+                object = "ls_kitcol2_counter4_a",
+                price = 750,
+            },
+            {
+                label = "Counter Type 4 B",
+                object = "ls_kitcol2_counter4_b",
+                price = 750,
+            },
+            {
+                label = "Counter Type 4 C",
+                object = "ls_kitcol2_counter4_c",
+                price = 750,
+            },
+            {
+                label = "Counter Type 4 D",
+                object = "ls_kitcol2_counter4_d",
+                price = 750,
+            },
+            {
+                label = "Counter Type 5 A",
+                object = "ls_kitcol2_counter5_a",
+                price = 750,
+            },
+            {
+                label = "Counter Type 5 B",
+                object = "ls_kitcol2_counter5_b",
+                price = 750,
+            },
+            {
+                label = "Counter Type 5 C",
+                object = "ls_kitcol2_counter5_c",
+                price = 750,
+            },
+            {
+                label = "Counter Type 5 D",
+                object = "ls_kitcol2_counter5_d",
+                price = 750,
+            },
+            {
+                label = "Counter Type 6 A",
+                object = "ls_kitcol2_counter6_a",
+                price = 750,
+            },
+            {
+                label = "Counter Type 6 B",
+                object = "ls_kitcol2_counter6_b",
+                price = 750,
+            },
+            {
+                label = "Counter Type 6 C",
+                object = "ls_kitcol2_counter6_c",
+                price = 750,
+            },
+            {
+                label = "Counter Type 6 D",
+                object = "ls_kitcol2_counter6_d",
+                price = 750,
+            },
+            {
+                label = "Counter Type 7 A",
+                object = "ls_kitcol2_counter7_a",
+                price = 750,
+            },
+            {
+                label = "Counter Type 7 B",
+                object = "ls_kitcol2_counter7_b",
+                price = 750,
+            },
+            {
+                label = "Counter Type 7 C",
+                object = "ls_kitcol2_counter7_c",
+                price = 750,
+            },
+            {
+                label = "Counter Type 7 D",
+                object = "ls_kitcol2_counter7_d",
+                price = 750,
+            },
+            {
+                label = "Counter Type 8 A",
+                object = "ls_kitcol2_counter8_a",
+                price = 750,
+            },
+            {
+                label = "Counter Type 8 B",
+                object = "ls_kitcol2_counter8_b",
+                price = 750,
+            },
+            {
+                label = "Counter Type 8 C",
+                object = "ls_kitcol2_counter8_c",
+                price = 750,
+            },
+            {
+                label = "Counter Type 8 D",
+                object = "ls_kitcol2_counter8_d",
+                price = 750,
+            },
+            {
+                label = "Counter Type 9 A",
+                object = "ls_kitcol2_counter9_a",
+                price = 750,
+            },
+            {
+                label = "Counter Type 9 B",
+                object = "ls_kitcol2_counter9_b",
+                price = 750,
+            },
+            {
+                label = "Counter Type 9 C",
+                object = "ls_kitcol2_counter9_c",
+                price = 750,
+            },
+            {
+                label = "Counter Type 9 D",
+                object = "ls_kitcol2_counter9_d",
+                price = 750,
+            },
+            {
+                label = "Island Type 1 A",
+                object = "ls_kitcol2_islaco1_a",
+                price = 750,
+            },
+            {
+                label = "Island Type 1 B",
+                object = "ls_kitcol2_islaco1_b",
+                price = 750,
+            },
+            {
+                label = "Island Type 1 C",
+                object = "ls_kitcol2_islaco1_c",
+                price = 750,
+            },
+            {
+                label = "Island Type 1 D",
+                object = "ls_kitcol2_islaco1_d",
+                price = 750,
+            },
+            {
+                label = "Island Type 2 A",
+                object = "ls_kitcol2_islaco2_a",
+                price = 750,
+            },
+            {
+                label = "Island Type 2 B",
+                object = "ls_kitcol2_islaco2_b",
+                price = 750,
+            },
+            {
+                label = "Island Type 2 C",
+                object = "ls_kitcol2_islaco2_c",
+                price = 750,
+            },
+            {
+                label = "Island Type 2 D",
+                object = "ls_kitcol2_islaco2_d",
+                price = 750,
+            },
+            {
+                label = "Island Type 3 A",
+                object = "ls_kitcol2_islaco3_a",
+                price = 750,
+            },
+            {
+                label = "Island Type 3 B",
+                object = "ls_kitcol2_islaco3_b",
+                price = 750,
+            },
+            {
+                label = "Island Type 3 C",
+                object = "ls_kitcol2_islaco3_c",
+                price = 750,
+            },
+            {
+                label = "Island Type 3 D",
+                object = "ls_kitcol2_islaco3_d",
+                price = 750,
+            },
+            {
+                label = "Island Type 4 A",
+                object = "ls_kitcol2_islaco4_a",
+                price = 750,
+            },
+            {
+                label = "Island Type 4 B",
+                object = "ls_kitcol2_islaco4_b",
+                price = 750,
+            },
+            {
+                label = "Island Type 4 C",
+                object = "ls_kitcol2_islaco4_c",
+                price = 750,
+            },
+            {
+                label = "Island Type 4 D",
+                object = "ls_kitcol2_islaco4_d",
+                price = 750,
+            },
+            {
+                label = "Island Type 5 A",
+                object = "ls_kitcol2_islaco5_a",
+                price = 750,
+            },
+            {
+                label = "Island Type 5 B",
+                object = "ls_kitcol2_islaco5_b",
+                price = 750,
+            },
+            {
+                label = "Island Type 5 C",
+                object = "ls_kitcol2_islaco5_c",
+                price = 750,
+            },
+            {
+                label = "Island Type 5 D",
+                object = "ls_kitcol2_islaco5_d",
+                price = 750,
+            },
+            {
+                label = "Island Type 6 A",
+                object = "ls_kitcol2_islaco6_a",
+                price = 750,
+            },
+            {
+                label = "Island Type 6 B",
+                object = "ls_kitcol2_islaco6_b",
+                price = 750,
+            },
+            {
+                label = "Island Type 6 C",
+                object = "ls_kitcol2_islaco6_c",
+                price = 750,
+            },
+            {
+                label = "Island Type 6 D",
+                object = "ls_kitcol2_islaco6_d",
+                price = 750,
+            },
+            {
+                label = "Island Type 7 A",
+                object = "ls_kitcol2_islaco7_a",
+                price = 750,
+            },
+            {
+                label = "Island Type 7 B",
+                object = "ls_kitcol2_islaco7_b",
+                price = 750,
+            },
+            {
+                label = "Island Type 7 C",
+                object = "ls_kitcol2_islaco7_c",
+                price = 750,
+            },
+            {
+                label = "Island Type 7 D",
+                object = "ls_kitcol2_islaco7_d",
+                price = 750,
+            },
+        }
+    },

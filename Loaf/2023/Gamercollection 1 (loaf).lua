f55f707a4b1f296c39a0f13dd897e92e762add2a
LunaticStudio <108892318+LunaticStudio@users.noreply.github.com>
2023-06-02 17:05:10 -0300

    loaf gamer

---
+++ Loaf/2023/Gamercollection 1 (loaf).lua
@@ -0,0 +1,595 @@
+    {
+        label = "Gamer Collection 1",
+        furniture = {
+            {
+                label = "Aliens Decoration",
+                object = "ls_gaymer_alien",
+                price = 750,
+            },
+            {
+                label = "Arcade A",
+                object = "ls_gaymer_arcade1_a",
+                price = 750,
+            },
+            {
+                label = "Arcade b",
+                object = "ls_gaymer_arcade1_b",
+                price = 750,
+            },
+            {
+                label = "Arcade C",
+                object = "ls_gaymer_arcade1_c",
+                price = 750,
+            },
+            {
+                label = "Arcade D",
+                object = "ls_gaymer_arcade1_d",
+                price = 750,
+            },
+            {
+                label = "Arcade E",
+                object = "ls_gaymer_arcade1_e",
+                price = 750,
+            },
+            {
+                label = "Arcade F",
+                object = "ls_gaymer_arcade1_f",
+                price = 750,
+            },
+            {
+                label = "Arcade G",
+                object = "ls_gaymer_arcade1_g",
+                price = 750,
+            },
+            {
+                label = "Arcade H",
+                object = "ls_gaymer_arcade1_h",
+                price = 750,
+            },
+            {
+                label = "(New) Xbox 360",
+                object = "ls_gaymer_box_360",
+                price = 750,
+            },
+            {
+                label = "(New) ps3",
+                object = "ls_gaymer_box_ps3",
+                price = 750,
+            },
+            {
+                label = "(New) ps4 - A",
+                object = "ls_gaymer_box_ps4_a",
+                price = 750,
+            },
+            {
+                label = "(New) ps4 - B",
+                object = "ls_gaymer_box_ps4_b",
+                price = 750,
+            },
+            {
+                label = "(New) ps4 - C",
+                object = "ls_gaymer_box_ps4_c",
+                price = 750,
+            },
+            {
+                label = "(New) ps4 - D",
+                object = "ls_gaymer_box_ps4_d",
+                price = 750,
+            },
+            {
+                label = "(New) ps4 - E",
+                object = "ls_gaymer_box_ps4_e",
+                price = 750,
+            },
+            {
+                label = "(New) ps5",
+                object = "ls_gaymer_box_ps5",
+                price = 750,
+            },
+            {
+                label = "(New) Xbox One",
+                object = "ls_gaymer_box_xone",
+                price = 750,
+            },
+            {
+                label = "(New) Switch A",
+                object = "ls_gaymer_boxswitch_a",
+                price = 750,
+            },
+            {
+                label = "(New) Switch B",
+                object = "ls_gaymer_boxswitch_b",
+                price = 750,
+            },
+            {
+                label = "(New) Switch C",
+                object = "ls_gaymer_boxswitch_c",
+                price = 750,
+            },
+            {
+                label = "(New) Switch D",
+                object = "ls_gaymer_boxswitch_d",
+                price = 750,
+            },
+            {
+                label = "(New) Switch E",
+                object = "ls_gaymer_boxswitch_e",
+                price = 750,
+            },
+            {
+                label = "(New) Switch F",
+                object = "ls_gaymer_boxswitch_f",
+                price = 750,
+            },
+            {
+                label = "Cabinet Gamer",
+                object = "ls_gaymer_cabinet",
+                price = 750,
+            },
+            {
+                label = "Gamer Chair A",
+                object = "ls_gaymer_chair_a",
+                price = 750,
+            },
+            {
+                label = "Gamer Chair B",
+                object = "ls_gaymer_chair_b",
+                price = 750,
+            },
+            {
+                label = "Gamer Chair C",
+                object = "ls_gaymer_chair_c",
+                price = 750,
+            },
+            {
+                label = "Gamer Chair D",
+                object = "ls_gaymer_chair_d",
+                price = 750,
+            },
+            {
+                label = "Gamer Chair E",
+                object = "ls_gaymer_chair_e",
+                price = 750,
+            },
+            {
+                label = "Gamer Chair F",
+                object = "ls_gaymer_chair_f",
+                price = 750,
+            },
+            {
+                label = "Gamer Chair G",
+                object = "ls_gaymer_chair_g",
+                price = 750,
+            },
+            {
+                label = "Gamer Chair H",
+                object = "ls_gaymer_chair_h",
+                price = 750,
+            },
+            {
+                label = "Nintendo DS",
+                object = "ls_gaymer_ds",
+                price = 750,
+            },
+            {
+                label = "Super Nintendo",
+                object = "ls_gaymer_famicon",
+                price = 750,
+            },
+            {
+                label = "Sonic - MegaDrive Game",
+                object = "ls_gaymer_fitamega_a",
+                price = 750,
+            },
+            {
+                label = "Sonic - MegaDrive Game",
+                object = "ls_gaymer_fitamega_b",
+                price = 750,
+            },
+            {
+                label = "Sonic - MegaDrive Game",
+                object = "ls_gaymer_fitamega_c",
+                price = 750,
+            },
+            {
+                label = "Sonic - MegaDrive Game",
+                object = "ls_gaymer_fitamega_d",
+                price = 750,
+            },
+            {
+                label = "Sonic - MegaDrive Game",
+                object = "ls_gaymer_fitamega_e",
+                price = 750,
+            },
+            {
+                label = "Sonic - MegaDrive Game",
+                object = "ls_gaymer_fitamega_f",
+                price = 750,
+            },
+            {
+                label = "Sonic - MegaDrive Game",
+                object = "ls_gaymer_fitamega_g",
+                price = 750,
+            },
+            {
+                label = "Game Gear A",
+                object = "ls_gaymer_gamegear_a",
+                price = 750,
+            },
+            {
+                label = "Game Gear B",
+                object = "ls_gaymer_gamegear_b",
+                price = 750,
+            },
+            {
+                label = "Game Gear C",
+                object = "ls_gaymer_gamegear_c",
+                price = 750,
+            },
+            {
+                label = "Game Gear D",
+                object = "ls_gaymer_gamegear_d",
+                price = 750,
+            },
+            {
+                label = "Game Gear E",
+                object = "ls_gaymer_gamegear_e",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced A",
+                object = "ls_gaymer_gba_a",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced B",
+                object = "ls_gaymer_gba_b",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced C",
+                object = "ls_gaymer_gba_c",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced D",
+                object = "ls_gaymer_gba_d",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced E",
+                object = "ls_gaymer_gba_e",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced F",
+                object = "ls_gaymer_gba_f",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced G",
+                object = "ls_gaymer_gba_g",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced H",
+                object = "ls_gaymer_gba_h",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced I",
+                object = "ls_gaymer_gba_i",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced J",
+                object = "ls_gaymer_gba_j",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced K",
+                object = "ls_gaymer_gba_k",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced L",
+                object = "ls_gaymer_gba_l",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced M",
+                object = "ls_gaymer_gba_m",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced N",
+                object = "ls_gaymer_gba_n",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced O",
+                object = "ls_gaymer_gba_o",
+                price = 750,
+            },
+            {
+                label = "Game Boy Advanced P",
+                object = "ls_gaymer_gba_p",
+                price = 750,
+            },
+            {
+                label = "Gta V Boxed",
+                object = "ls_gaymer_gtavbox",
+                price = 750,
+            },
+            {
+                label = "Choosing Hat",
+                object = "ls_gaymer_hphat",
+                price = 750,
+            },
+            {
+                label = "Magazines",
+                object = "ls_gaymer_magazine",
+                price = 750,
+            },
+            {
+                label = "Mega Drive A",
+                object = "ls_gaymer_megadrive_a",
+                price = 750,
+            },
+            {
+                label = "Mega Drive B",
+                object = "ls_gaymer_megadrive_b",
+                price = 750,
+            },
+            {
+                label = "Pokeball -  N Switch",
+                object = "ls_gaymer_pokeball",
+                price = 750,
+            },
+            {
+                label = "Projector Screen",
+                object = "ls_gaymer_projectorscreen",
+                price = 750,
+            },
+            {
+                label = "Ps2 A",
+                object = "ls_gaymer_ps2_a",
+                price = 750,
+            },
+            {
+                label = "Ps2 B",
+                object = "ls_gaymer_ps2_b",
+                price = 750,
+            },
+            {
+                label = "Ps2 C",
+                object = "ls_gaymer_ps2_c",
+                price = 750,
+            },
+            {
+                label = "Ps4",
+                object = "ls_gaymer_ps4",
+                price = 750,
+            },
+            {
+                label = "Ps4 Addon",
+                object = "ls_gaymer_ps4_addon_a",
+                price = 750,
+            },
+            {
+                label = "Ps4 Addon B",
+                object = "ls_gaymer_ps4_addon_b",
+                price = 750,
+            },
+            {
+                label = "Ps4 Addon C",
+                object = "ls_gaymer_ps4_addon_c",
+                price = 750,
+            },
+            {
+                label = "Ps5 A",
+                object = "ls_gaymer_ps5_a",
+                price = 750,
+            },
+            {
+                label = "Ps5 B",
+                object = "ls_gaymer_ps5_b",
+                price = 750,
+            },
+            {
+                label = "Ps5 C",
+                object = "ls_gaymer_ps5_c",
+                price = 750,
+            },
+            {
+                label = "Ps5 D",
+                object = "ls_gaymer_ps5_d",
+                price = 750,
+            },
+            {
+                label = "Ps5 E",
+                object = "ls_gaymer_ps5_e",
+                price = 750,
+            },
+            {
+                label = "Ps5 F",
+                object = "ls_gaymer_ps5_f",
+                price = 750,
+            },
+            {
+                label = "Ps5 Dock",
+                object = "ls_gaymer_ps5dock",
+                price = 750,
+            },
+            {
+                label = "R2D2",
+                object = "ls_gaymer_r2d2_a",
+                price = 750,
+            },
+            {
+                label = "Series X A",
+                object = "ls_gaymer_seriesx_a",
+                price = 750,
+            },
+            {
+                label = "Series X B",
+                object = "ls_gaymer_seriesx_b",
+                price = 750,
+            },
+            {
+                label = "Series X C",
+                object = "ls_gaymer_seriesx_c",
+                price = 750,
+            },
+            {
+                label = "Series X D",
+                object = "ls_gaymer_seriesx_d",
+                price = 750,
+            },
+            {
+                label = "Series X E",
+                object = "ls_gaymer_seriesx_e",
+                price = 750,
+            },
+            {
+                label = "Series X F",
+                object = "ls_gaymer_seriesx_f",
+                price = 750,
+            },
+            {
+                label = "Switch A",
+                object = "ls_gaymer_switch_a",
+                price = 750,
+            },
+            {
+                label = "Switch Addon A",
+                object = "ls_gaymer_switch_addon1_a",
+                price = 750,
+            },
+            {
+                label = "Switch Addon B",
+                object = "ls_gaymer_switch_addon1_b",
+                price = 750,
+            },
+            {
+                label = "Switch Addon C",
+                object = "ls_gaymer_switch_addon1_c",
+                price = 750,
+            },
+            {
+                label = "Switch Addon D",
+                object = "ls_gaymer_switch_addon1_d",
+                price = 750,
+            },
+            {
+                label = "Switch Addon E",
+                object = "ls_gaymer_switch_addon1_e",
+                price = 750,
+            },
+            {
+                label = "Switch Addon F",
+                object = "ls_gaymer_switch_addon1_f",
+                price = 750,
+            },
+            {
+                label = "Switch Addon X",
+                object = "ls_gaymer_switch_addon1_x",
+                price = 750,
+            },
+            {
+                label = "Switch B",
+                object = "ls_gaymer_switch_b",
+                price = 750,
+            },
+            {
+                label = "Switch C",
+                object = "ls_gaymer_switch_c",
+                price = 750,
+            },
+            {
+                label = "Switch D",
+                object = "ls_gaymer_switch_d",
+                price = 750,
+            },
+            {
+                label = "Switch E",
+                object = "ls_gaymer_switch_e",
+                price = 750,
+            },
+            {
+                label = "Switch Screen protector A",
+                object = "ls_gaymer_switch_screen_a",
+                price = 750,
+            },
+            {
+                label = "Switch Screen protector B",
+                object = "ls_gaymer_switch_screen_b",
+                price = 750,
+            },
+            {
+                label = "Switch Screen protector C",
+                object = "ls_gaymer_switch_screen_c",
+                price = 750,
+            },
+            {
+                label = "Switch Wheel",
+                object = "ls_gaymer_switch_wheel",
+                price = 750,
+            },
+            {
+                label = "Switch Dock",
+                object = "ls_gaymer_switchdock",
+                price = 750,
+            },
+            {
+                label = "Tetris Wall Decoration",
+                object = "ls_gaymer_tetris",
+                price = 750,
+            },
+            {
+                label = "Vader SpaceShip",
+                object = "ls_gaymer_vader",
+                price = 750,
+            },
+            {
+                label = "Xbox 360",
+                object = "ls_gaymer_xbox360",
+                price = 750,
+            },
+            {
+                label = "Xbox One",
+                object = "ls_gaymer_xone",
+                price = 750,
+            },
+            {
+                label = "Xbox Series S",
+                object = "ls_gaymer_xseriess_a",
+                price = 750,
+            },
+            {
+                label = "Xbox Series B",
+                object = "ls_gaymer_xseriess_b",
+                price = 750,
+            },
+            {
+                label = "Xbox Series C",
+                object = "ls_gaymer_xseriess_c",
+                price = 750,
+            },
+            {
+                label = "Xbox Series D",
+                object = "ls_gaymer_xseriess_d",
+                price = 750,
+            },
+            {
+                label = "Xbox Series E",
+                object = "ls_gaymer_xseriess_e",
+                price = 750,
+            },
+            {
+                label = "Xbox Series F",
+                object = "ls_gaymer_xseriess_f",
+                price = 750,
+            },
+        },
+    }
